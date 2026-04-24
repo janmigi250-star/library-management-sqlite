3a57730d10c554bb6f962e649b6bccfbcf7bb1b6
JANVIER The Last <janmigi>
2026-04-24 10:54:45 +0200

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,56 +1,39 @@
 import express from "express";
-import { db } from "./config/sqlite.js";
-// import { connectToDb } from './config/db.js';
+import { db } from "./config/db.js";
 import cors from "cors";
 
 const PORT = 3000;
 const app = express();
-
-app.use(cors({
-  origin:["http://127.0.0.1:5500"],
-  methods:["GET","POST","PUT","DELETE"],
-  allowedHeaders:["Content-Type"]
-}))
 app.use(express.json());
-// connectToDb()
+app.use(cors({
+  origin:'http://127.0.0.1:5500',
+  methods:['GET','POST','PUT','DELETE'],
+  allowedHeaders:['Content-Type']
+}))
 
-app.post("/api/books", (req, res) => {
-  console.log(req.body);
-  
-  const { name, description, author, price, imageSrc } = req.body;
-  if (!name || !description || !author || !price) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-  const query = `INSERT INTO books (name, description, author, price,imageSrc) VALUES (?, ?, ?, ?,?)`;
-  db.run(query, [name, description, author, price, imageSrc || ""], (err) => {
-    if (err) {
-      return console.log("error in creating books", err);
-    }
-    console.log("inserted books in db successfully.");
-    return res.status(201).json({ message: "Created successfully" })
-  });
-});
 
-app.get("/api/books", (req, res) => {
-  const query = `SELECT * FROM books`;
-  db.all(query, [], (err, rows) => {
-    if (err) {
-      return console.log("error in fetching books", err);
+app.post('/api/books',(req,res)=>{
+    const { name, description, author, price, imageSrc } = req.body;
+    if(!name || !description || !author || !price || !imageSrc){
+      console.log('all fields are riquired');
+      return res.status(400).json('all fields are riquired')
     }
-    return res.status(200).json(rows);
-  });
-});
 
-app.delete("/api/books/:id", (req, res) => {
-  const { id } = req.params;
-  const query = `DELETE FROM books WHERE id = ?`;
-  db.run(query, [id], (err) => {
-    if (err) {
-      return res.status(500).json({ message: "Error deleting book", error: err });
-    }
-    return res.status(200).json({ message: "Book deleted successfully" });
-  });
-});
+    const query = `INSERT INTO books (name, description, author, price, imageSrc) VALUES(?,?,?,?)`
+    db.run(query,[name, description, author, price, imageSrc],(err)=>{
+      if(err){
+        return console.log('error in creating book',err);
+        
+      }
+      return res.status(201).json({message:'book created susseccfull'})
+    })
+
+}) 
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`);
